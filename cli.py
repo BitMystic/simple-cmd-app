fc94d729dc33c1d4bcdb2117450ef36f995c0643
bitmystic <hardaya.singh@uon.edu.au>
2023-08-30 15:22:57 +1000

    clean up and added functionality of listing all unique file extentions

--- cli.py
+++ cli.py
@@ -1,32 +1,45 @@
-# search.py - CLI-Utilities - play4Tutorials.hashnode.dev, github.com/play4Tutorials
+import re
+
 try:
-    import os # stdlib
-    import sys # stdlib
-# Exception and Error Handling
+    import os 
+    import sys
 except ImportError as e:
     print("Error: Some modules could not be imported from stdlib('sys' and 'os')")
 
-keyword = sys.argv[1] # Get the keyword that is to be searched for
-path = " ".join(sys.argv[2:]) # Get the path where to search for the keyword
+keyword = sys.argv[1] 
+path = " ".join(sys.argv[2:]) 
 
-filesMatched = 0 # Initialize filesMatched as 0
+filesMatched = 0
 
-# Function to search the keyword in a given directory
 def search_files(keyword, path):
     filesFound = filesMatched
-    for root, dirs, files in os.walk(path): # Use os.walk(path) to loop through the given directory and get the root, dirs, and files
-        for file in files: # Loop through all the files in the given directory.
-            if keyword in file: # Check if keyword is in filename
-                filesFound += 1 # Counter to see how many files matched
-                print(" "+root+'/'+str(file)+"\n") # Print out the full file path for every matching file
-                if filesFound == 0: # Check if no files matched the given keyword and print the same
+    for root, dirs, files in os.walk(path):
+        for file in files: 
+            if keyword in file:
+                filesFound += 1
+                print(" "+root+'/'+str(file)+"\n") 
+                if filesFound == 0: 
                     print("No matching files in the given directory! Try another directory!")
-    # If 1 or more files are matched then print the same
     print(f"No. of files matched: {filesFound}")
 
+def overview(path):
+    extentions = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            try:
+                str_start_pos = re.search(r'\.', file).start() + 1
+                ext = file[str_start_pos:]
+                if ext not in extentions:
+                    extentions.append(ext)
+            except:
+                continue
+    return extentions
 try:
-    search_files(keyword, path) # Call the function
-# Error and Exception Handling
+    search_files(keyword, path) 
+    extentions = overview(path)
+    print("All unique extensions found in this dir: ")
+    for i in extentions:
+        print(i)
 except FileNotFoundError as e:
     print("Error: FileNotFoundError occured! Make sure you entered the correct path and entered it correctly.")
 except Exception as e:
